2cdc144550e2e5ca5dd8aff59e795b13063927cf
Froment Chloe <fromentchloe.web@gmail.com>
2023-04-20 15:54:40 +0200

    [funt]add header]

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Le Quai Antique</title>
+    <meta charset="UTF-8">
+    <meta name="description" content= "Découvrez le troisième restaurant de burger du Chef Michant à Chambéry ! Nous vous proposons une large sélection de burgers savoureux, préparés avec des ingrédients frais et de qualité. Notre équipe chaleureuse et accueillante sera heureuse de vous recevoir pour un déjeuner ou un dîner délicieux. Découvrez notre carte en ligne et réservez votre table dès maintenant !">
+    <meta name="keywords" content="restaurant, restaurant savoyard, burger, chef">
+    <meta name="author" content="Froment Chloe">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/views/style/main.css">
+</head>
+<body>
+  <header>
+    <?php 
+       require 'views/header.html';
+    ?>
+  </header>
+
+
+
+
+
+
+
+
+
+
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+  <script src="js/script.js"></script>
+</body>
+</html>
+
+
--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-$(function() {
-    
-});
+
+    var menuIcon = document.querySelector('.menu-icon');
+    var menu = document.querySelector('.menu');
+    menuIcon.addEventListener('click', function() {
+      this.classList.toggle('active');
+      menu.classList.toggle('active');
+    });
