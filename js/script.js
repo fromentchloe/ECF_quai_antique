950e60677acabe9579bb6f075f4d7bd497e25823
Froment Chloe <fromentchloe.web@gmail.com>
2023-04-21 18:32:11 +0200

    [ref]work fof the wrapper

--- js/script.js
+++ js/script.js
@@ -5,9 +5,6 @@ navbarToggler.addEventListener('click', function () {
   navbarCollapse.classList.toggle('show');
 });
 
-// Animation reservation 
-var reservation = document.getElementById("resa-btn");
 
-setInterval(function() {
-  reservation.style.visibility =(reservation.style.visibility == 'hiden') ? 'visible' : 'hidden';
-}, 500);
+
+
