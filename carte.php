5a1c2e213c279ed0a2412e593673a387706cf9fc
Froment Chloe <fromentchloe.web@gmail.com>
2023-05-02 16:23:29 +0200

    [add]add js on carte.php page

--- carte.php
+++ carte.php
@@ -20,4 +20,8 @@
 <footer>
   <?php include "views/footer.html"; ?>
 </footer>
-</body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
