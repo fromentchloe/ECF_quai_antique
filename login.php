6a3b1342bece32c4d3d2a35c0dc5f0a6405c04f5
Froment Chloe <fromentchloe.web@gmail.com>
2023-05-03 20:51:34 +0200

    [conn]database connection to add a customer

--- login.php
+++ login.php
@@ -2,40 +2,38 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h2 class="modal-title" id="login-modal-label">Se connecter</h2>
+        <h3 class="modal-title" id="login-modal-label">Se connecter</h3>
         <button type="button" class="btn-close" id="close"></button>
       </div>
       <div class="modal-body">
-      <form method="POST" action="login_prosess.php">
-          <label for="email">Email :</label>
-          <input type="email" id="email" name="email" required><br><br>
+        <form method="POST" action="login_process.php">
+          <label for="login-email">Email :</label>
+          <input type="email" id="login-email" name="email" required><br><br>
           <label for="password">Mot de passe :</label>
           <input type="password" id="password" name="password" required><br><br>
           <input class="submit" type="submit" value="Se connecter"></input>
         </form>
-        <h3 class="m-4"> Pas Encore de compte...</h3>
+        <h3 class="m-4">Pas encore de compte...</h3>
         <form class="signup-form" action="signup_process.php" method="POST">
-		<label  for="name">Nom :</label>
-		<input  type="text" id="name" name="name" required>
-
-		<label for="email">Email :</label>
-		<input type="email" id="email" name="email" required>
-
-		<label for="password">Mot de passe :</label>
-		<input type="password" id="password" name="password_hash" required>
-
-		<label for="allergies">Allergies (optionnel) :</label>
-		<textarea id="allergies" name="allergies"></textarea>
-
-		<input class="submit" type="submit" value="S'inscrire">
-		<input class="cancel" type="reset" value="Annuler">
-	</form>
-
+          <label for="signup-name">Nom :</label>
+          <input type="text" id="signup-name" name="name" required>
+      
+          <label for="signup-email">Email :</label>
+          <input type="email" id="signup-email" name="email" required>
+      
+          <label for="signup-password">Mot de passe :</label>
+          <input type="password" id="signup-password" name="password_hash" required>
+      
+          <label for="allergies">Allergies (optionnel) :</label>
+          <textarea id="allergies" name="allergies"></textarea>
+      
+          <input class="submit" type="submit" value="S'inscrire">
+          <input class="cancel" type="reset" value="Annuler">
+        </form>
       </div>
     </div>
   </div>
 </div>
-
 
 
 
--- signup_process.php
+++ signup_process.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Le Quai Antique</title>
+    <meta charset="UTF-8">
+  </head>
+  <body style="background-color: #e6ccb2; color: #7f5539; text-align: center"> 
+    <a href="./index.php"><img src="./style/image/Logo.png"></a>
 <?php
 // Connexion à la base de données
 require_once './MySQL/connection_bdd.php';
@@ -10,20 +18,27 @@ $password = mysqli_real_escape_string($conn, $_POST['password_hash']);
 $password_hash = password_hash($password, PASSWORD_DEFAULT); // Hasher le mot de passe
 $allergies = mysqli_real_escape_string($conn, $_POST['allergies']);
 
-// Requête SQL pour insérer un nouvel utilisateur dans la base de données
-$sql = "INSERT INTO users (name, email, password_hash, allergies) VALUES ('$name', '$email', '$password_hash', '$allergies')";
-if (mysqli_query($conn, $sql)) {
-    // L'inscription a réussi, stocker le nom de l'utilisateur dans la session
-    session_start();
-    $_SESSION['user_name'] = $name;
-    $_SESSION['user_email'] = $email;
-    $_SESSION['user_is_admin'] = false;
-    header('Location: index.php');
+// Vérifier si l'utilisateur existe déjà en base de données
+$sql = "SELECT COUNT(*) FROM users WHERE email = '$email'";
+$result = mysqli_query($conn, $sql);
+$count = mysqli_fetch_array($result)[0];
+if ($count > 0) {
+    echo '<h2>Un compte existe déja a l\'adresse ' . $email . ' </h2><br><a style="background-color: #7f5539; color: #e6ccb2; border-radius: 20px; padding: 10px 20px; font-size: 25px; cursor: pointer; text-decoration:none" href="javascript:history.go(-1)">Page précédente</a></div>';
 } else {
-    // L'inscription a échoué, afficher un message d'erreur
-    echo "Erreur : " . mysqli_error($conn);
+    // Requête SQL pour insérer un nouvel utilisateur dans la base de données
+    $sql = "INSERT INTO users (name, email, password_hash, allergies) VALUES ('$name', '$email', '$password_hash', '$allergies')";
+    if (mysqli_query($conn, $sql)) {
+        // L'inscription a réussi, stocker le nom de l'utilisateur dans la session
+        session_start();
+        $_SESSION['user_name'] = $name;
+        $_SESSION['user_email'] = $email;
+        $_SESSION['user_is_admin'] = false;
+        header('Location: index.php');
+    } else {
+        // L'inscription a échoué, afficher un message d'erreur
+        echo "Erreur : " . mysqli_error($conn);
+    }
 }
-
 
 // Fermer la connexion à la base de données
 mysqli_close($conn);
