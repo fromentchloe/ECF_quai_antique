691ff03093902ec9d2b789005fb05c962af6bb62
Froment Chloe <fromentchloe.web@gmail.com>
2023-05-22 16:28:47 +0200

    [add] online

--- MySQL/connection_bdd.php
+++ MySQL/connection_bdd.php
@@ -1,11 +1,12 @@
 <?php 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "quaiavare";
+$servername = getenv('	n2o93bb1bwmn0zle.chr7pe7iynqr.eu-west-1.rds.amazonaws.com');
+$username = getenv('p0vgmuoauk538bk1');
+$password = getenv('vzb3f0z66b3252cs');
+$dbname = getenv('d9ujfqoevkamlvp7');
 
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
