bf91a2b5f200b11a53ab3eef13d5161f51033050
Froment Chloe <fromentchloe.web@gmail.com>
2023-05-22 19:58:02 +0200

    composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/quaiantiquerestaurant',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '60b3c042e2c033c8b120b65c2f77e6301a806054',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'components/jquery' => array(
+            'pretty_version' => '3.6.0',
+            'version' => '3.6.0.0',
+            'reference' => '6cf38ee1fd04b6adf8e7dda161283aa35be818c3',
+            'type' => 'component',
+            'install_path' => __DIR__ . '/../components/jquery',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'vendor/quaiantiquerestaurant' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '60b3c042e2c033c8b120b65c2f77e6301a806054',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
