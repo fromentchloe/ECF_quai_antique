05d5cd5f9f8a3ef713ba6e07fc5e8bad246534ea
Froment Chloe <fromentchloe.web@gmail.com>
2023-05-23 19:12:46 +0200

    [conn]

--- MySQL/connection_bdd.php
+++ MySQL/connection_bdd.php
@@ -1,18 +1,4 @@
 <?php
-if (getenv('JAWSDB_URL') !== false) {
-    $url = getenv('JAWSDB_URL');
-    $dbparts = parse_url($url);
-    
-    $hostname = $dbparts['host'];
-    $username = $dbparts['user'];
-    $password = $dbparts['pass'];
-    $database = ltrim($dbparts['path'],'/');
-
-    $conn = mysqli_connect($hostname, $username, $password, $database);
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-} else {
     $servername = "localhost";
     $username = "root";
     $password = "";
@@ -22,5 +8,98 @@ if (getenv('JAWSDB_URL') !== false) {
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
+    $url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['	ble5mmo2o5v9oouq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'];
+$username = $dbparts['yaqgmhaawa2mofqz'];
+$password = $dbparts['iakckq5tsei51ap0'];
+$database = ltrim($dbparts['c2ltqnyl5eus2goc'],'/');
+   
+    $conn_dist = mysqli_connect($hostname, $username, $password, $database );
+
+if (!$conn_dist) {
+    die("Erreur de connexion à la base de données en ligne : " . mysqli_connect_error());
+}
+
+
+
+
+// Exemple : Sélectionnez des données depuis la base de données locale
+$localQuery = "SELECT * FROM table_name";
+$localResult = mysqli_query($localConn, $localQuery);
+
+if ($localResult) {
+    // Traitez les données locales ici
+}
+
+// Exemple : Sélectionnez des données depuis la base de données en ligne
+$remoteQuery = "SELECT * FROM table_name";
+$remoteResult = mysqli_query($remoteConn, $remoteQuery);
+
+if ($remoteResult) {
+    // Traitez les données en ligne ici
+}
+
+// N'oubliez pas de fermer les connexions lorsque vous avez terminé
+mysqli_close($localConn);
+mysqli_close($remoteConn);
+?>
 }
 ?>
+
+
+
+
+<?php
+// Connexion à la base de données locale "quaiavare"
+$localHost = 'localhost';
+$localUsername = 'username';
+$localPassword = 'password';
+$localDatabase = 'quaiavare';
+
+$localConn = new mysqli($localHost, $localUsername, $localPassword, $localDatabase);
+
+if ($localConn->connect_error) {
+    die("Erreur de connexion à la base de données locale : " . $localConn->connect_error);
+}
+
+// Connexion à la base de données en ligne "jawdb" sur Heroku
+$remoteHost = 'your-heroku-host';
+$remoteUsername = 'your-heroku-username';
+$remotePassword = 'your-heroku-password';
+$remotePort = 'your-heroku-port';
+$remoteDatabase = 'jawdb';
+
+$remoteConn = new mysqli($remoteHost, $remoteUsername, $remotePassword, $remoteDatabase, $remotePort);
+
+if ($remoteConn->connect_error) {
+    die("Erreur de connexion à la base de données en ligne : " . $remoteConn->connect_error);
+}
+
+// Vous avez maintenant deux connexions établies : $localConn pour la base de données locale "quaiavare"
+// et $remoteConn pour la base de données en ligne "jawdb" sur Heroku.
+
+// Vous pouvez exécuter des requêtes SQL et effectuer des opérations avec les deux connexions selon vos besoins.
+
+// Exemple : Sélectionnez des données depuis la base de données locale
+$localQuery = "SELECT * FROM table_name";
+$localResult = $localConn->query($localQuery);
+
+if ($localResult) {
+    // Traitez les données locales ici
+}
+
+// Exemple : Sélectionnez des données depuis la base de données en ligne
+$remoteQuery = "SELECT * FROM table_name";
+$remoteResult = $remoteConn->query($remoteQuery);
+
+if ($remoteResult) {
+    // Traitez les données en ligne ici
+}
+
+// N'oubliez pas de fermer les connexions lorsque vous avez terminé
+$localConn->close();
+$remoteConn->close();
+?>
+
